5fc6aeb9b3bd316544d20df0a55494a9e23a5aa7
nidu003 <108021671+nidu003@users.noreply.github.com>
2024-07-05 16:27:02 +0530

    Update ImageGrid component and globals.css for grid layout

--- app/components/ImageGrid.tsx
+++ app/components/ImageGrid.tsx
@@ -2,21 +2,23 @@ import Image from 'next/image';
 
 function ImageGrid() {
   return (
-    <div className="image-grid grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4">
-      <div className="image-item">
-        <Image src="/birthday.png" alt="Birthday" width={300} height={200} className="w-full h-auto" />
-      </div>
-      <div className="image-item">
-        <Image src="/flowers.png" alt="Flowers" width={300} height={200} className="w-full h-auto" />
-      </div>
-      <div className="image-item">
-        <Image src="/corporate.png" alt="Corporate" width={300} height={200} className="w-full h-auto" />
-      </div>
-      <div className="image-item">
-        <Image src="/wedding.png" alt="Weddings" width={300} height={200} className="w-full h-auto" />
-      </div>
-      <div className="image-item">
-        <Image src="/anniversary.png" alt="Anniversary" width={300} height={200} className="w-full h-auto" />
+    <div className="image-grid-i">
+      <div className="cont">
+        <div className="birthday">
+          <Image src="/birthday.png" alt="Birthday" width={500} height={375} className="w-full h-auto" />
+        </div>
+        <div className="flowers">
+          <Image src="/flowers.png" alt="Flowers" width={500} height={779} className="w-full h-auto" />
+        </div>
+        <div className="corporate">
+          <Image src="/corporate.png" alt="Corporate" width={500} height={375} className="w-full h-auto" />
+        </div>
+        <div className="weddings">
+          <Image src="/wedding.png" alt="Weddings" width={500} height={375} className="w-full h-auto" />
+        </div>
+        <div className="anniversary">
+          <Image src="/anniversary.png" alt="Anniversary" width={500} height={375} className="w-full h-auto" />
+        </div>
       </div>
     </div>
   );
