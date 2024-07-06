a6af4be4716061054d75e9dc9d5403569191b1d6
nidu003 <108021671+nidu003@users.noreply.github.com>
2024-07-06 19:17:37 +0530

    Update Do component and globals.css for styling and zoom effect

--- app/components/Do.tsx
+++ app/components/Do.tsx
@@ -5,39 +5,38 @@ const Do: React.FC = () => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-lg-4 col-md-6 mb-4">
+        <div className="col-md-4">
           <div className="card">
-            <Image src="/Personalized.png" className="card-img-top" alt="Personalized" width={400} height={300} />
+            <img src="/Personalized.png" className="card-img-top hover-effect-zoom-out" alt="Personalized" />
             <div className="card-body">
               <h5 className="card-title">Personalized</h5>
             </div>
           </div>
         </div>
-        <div className="col-lg-4 col-md-6 mb-4">
+        <div className="col-md-4">
           <div className="card">
-            <Image src="/Luxurious.png" className="card-img-top" alt="Luxurious" width={400} height={300} />
+            <img src="/Luxurious.png" className="card-img-top" alt="Luxurious" />
             <div className="card-body">
               <h5 className="card-title">Luxurious</h5>
             </div>
           </div>
         </div>
-        <div className="col-lg-4 col-md-6 mb-4">
+        <div className="col-md-4">
           <div className="card">
-            <Image src="/specialoccasion.png" className="card-img-top" alt="Special Occasion" width={400} height={300} />
+            <img src="/specialoccasion.png" className="card-img-top" alt="Special Occasion" />
             <div className="card-body">
               <h5 className="card-title">Special Occasion</h5>
             </div>
           </div>
         </div>
       </div>
-
       <h2 className="mt-5">Trending Now</h2>
-
-      <div className="row">
-        <div className="col-lg-4 col-md-6 mb-4">
+      <div className="row mt-3">
+        <div className="col-md-4">
           <div className="card">
-            <div className="card-img-top-2-container">
-              <Image src="/moneyenvelop.png" className="card-img-top-2 hover-effect-zoom-out" alt="Money Envelopes" width={400} height={300} />
+            <div className="card-img-top-2-container ">
+              <img src="/moneyenvelop.png" className="card-img-top-2 hover-effect-zoom-out" alt="Money Envelopes" />
+              <span className="discount-tag">40% off</span>
               <div className="overlay">
                 <h5 className="card-title">Money Envelopes</h5>
                 <a href="#" className="btn btn-primary">Shop Now</a>
@@ -45,10 +44,11 @@ const Do: React.FC = () => {
             </div>
           </div>
         </div>
-        <div className="col-lg-4 col-md-6 mb-4">
+        <div className="col-md-4">
           <div className="card">
             <div className="card-img-top-2-container">
-              <Image src="/waxseal.png" className="card-img-top-2 hover-effect-zoom-out" alt="Wax Seals" width={400} height={300} />
+              <img src="/waxseal.png" className="card-img-top-2 hover-effect-zoom-out" alt="Wax Seals" />
+              <span className="discount-tag">40% off</span>
               <div className="overlay">
                 <h5 className="card-title">Wax Seals</h5>
                 <a href="#" className="btn btn-primary">Shop Now</a>
@@ -56,10 +56,11 @@ const Do: React.FC = () => {
             </div>
           </div>
         </div>
-        <div className="col-lg-4 col-md-6 mb-4">
+        <div className="col-md-4">
           <div className="card">
             <div className="card-img-top-2-container">
-              <Image src="/gifttag.png" className="card-img-top-2 hover-effect-zoom-out" alt="Gift Tags" width={400} height={300} />
+              <img src="/gifttag.png" className="card-img-top-2 hover-effect-zoom-out" alt="Gift Tags" />
+              <span className="discount-tag">40% off</span>
               <div className="overlay">
                 <h5 className="card-title">Gift Tags</h5>
                 <a href="#" className="btn btn-primary">Shop Now</a>
